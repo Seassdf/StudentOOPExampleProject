6d65adbf7482240c20b38423e4c629485f17d7d3
ITSTEP\st <st>
2024-04-04 18:55:24 +0300

    create incapsulation

--- StudentOOPExampleProject/Manager.h
+++ StudentOOPExampleProject/Manager.h
@@ -5,25 +5,28 @@
 class Manager
 {
 public:
+
 	int getMaxMark(Student* students, int length) {
-		int max = students[0].mark;
+		int max = students[0].getMark();
 
 		for (int i = 0; i < length; i++)
 		{
-			if (students[i].mark < max) {
-				max = students[i].mark;
+			if (students[i].getMark() < max) {
+				max = students[i].getMark();
 			}
 		}
 
 		return max;
 	};
+
+
 	int getMinNum(Student* students, int length) {
-		int min = students[0].mark;
+		int min = students[0].getMark();
 
 		for (int i = 0; i < length; i++)
 		{
-			if (students[i].mark < min) {
-				min = students[i].mark;
+			if (students[i].getMark() < min) {
+				min = students[i].getMark();
 			}
 		}
 
@@ -31,10 +34,10 @@ public:
 	};
 
 	double calcAvgMark(Student* students, int length) {
-		int avg = students[0].mark;
+		int avg = students[0].getMark();
 		for (int i = 0; i < length; i++)
 		{
-			avg += students[i].mark;
+			avg += students[i].getMark();
 		}
 
 
@@ -47,7 +50,7 @@ public:
 
 		for (int i = 0; i < length; i++)
 		{
-			if (students[i].mark == bestMark) {
+			if (students[i].getMark() == bestMark) {
 				st = students[i];
 				break;
 			}
--- StudentOOPExampleProject/Source.cpp
+++ StudentOOPExampleProject/Source.cpp
@@ -1,22 +1,15 @@
-
 #include "Manager.h"
+#include <iostream>
 
 int main() {
 	Student st1("Alice", 19, 5);
-	Student st2{ "Jack",16,8 };
+	Student st2{ "Jack",-18,8 };
 	Student st3{ "Peter" ,17,10 };
 	Student st4{ "Anna",12,4 };
 
-	const int size = 4;
-	Student students[size]{ st1,st2,st3,st4 };
-
-	Manager manager;
-
-	cout << "Best mark is " << manager.getMaxMark(students, 4) << endl;
-	cout << "Best student is " << manager.getBestStudent(students, 4).convert() << endl;
-	//
-	//	cout << st1.convert() << endl;
-	//	cout << st2.convert() << endl;
-	//	cout << st3.convert() << endl;
-	//	cout << st4.convert() << endl;
+	
+		cout << st1.convert() << endl;
+		cout << st2.convert() << endl;
+		cout << st3.convert() << endl;
+		cout << st4.convert() << endl;
 }
